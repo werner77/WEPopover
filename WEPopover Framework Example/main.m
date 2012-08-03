aed72ec3336ee9ea724d05a255fbbe5753c6ac6a
José Expósito <jose.exposito89@gmail.com>
2012-08-03 13:11:22 +0200

    Added support for compile WEPopover as a Framework
    Is possible to compile WEPopover as a framework to add it to third
    party projects
    Specially useful to add to an ARC third party project without headaches
    Instructions in the README file
    Added a gitignore file

---
+++ WEPopover Framework Example/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WEPopover
+//
+//  Created by Werner Altewischer on 06/11/10.
+//  Copyright 2010 Werner IT Consultancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
